27fcd1167fd4c6acf3319b7e07842221fe3f4141
Daniël R <danielroth1890@gmail.com>
2021-03-20 20:45:39 +0100

    finish part 2.

---
+++ 02.Syntax-Practice/comparison_practice.js
@@ -0,0 +1,15 @@
+let age = 25;
+
+// loose comparison (verschillende types kunnen nog steeds gelijk zijn)
+
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log (age != 25);
+// console.log(age != '25');
+
+// strict comparison (verschillende types kunnen niet gelijk zijn)
+
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
---
+++ 02.Syntax-Practice/type_conversion_practice.js
@@ -0,0 +1,12 @@
+// type conversion
+let score = '100';
+
+console.log(Number(score) + 1);
+console.log(Number(score + 1));
+score = Number(score);
+console.log(score + 1);
+console.log(typeof score);
+
+//let result = Number('hello');
+let result = String(50)
+console.log(result, typeof result);
