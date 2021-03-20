b6fc6cfd810a1e50214739ae897fc3594f304ac8
Daniël R <danielroth1890@gmail.com>
2021-03-20 01:35:24 +0100

    finish array practice

---
+++ Deel_2/array_practice.js
@@ -0,0 +1,23 @@
+let ninjas = ['daniel', 'ryu', 'nigel'];
+
+// how to override an arraycel
+//ninjas[1] = 'ken';
+//console.log(ninjas[1]);
+
+//let ages = [20, 25, 30, 25];
+//console.log(ages[2]);
+
+// different kind of types in a single array
+//let random = ['shaun', 'daniel', 50, 10];
+//console.log(random);
+
+// array methods
+
+//let result = ninjas.join(',');
+//let result = ninjas.indexOf('chun-li');
+//let result = ninjas.concat(['ken', 'dirk']);
+let result = ninjas.push('ken');
+result = ninjas.pop();
+
+
+console.log(result);
