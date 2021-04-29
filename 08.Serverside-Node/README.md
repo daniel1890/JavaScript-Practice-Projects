c293ca0fbac26471e2cdf70d5afb28c2e2c08632
Daniël R <danielroth1890@gmail.com>
2021-04-29 02:05:51 +0200

    create necessary files, add express package

---
+++ 08.Serverside-Node/README.md
@@ -0,0 +1,10 @@
+# Serverside Werken Met Node.JS
+
+## Informatie
+
+In dit project is het de bedoeling om voor het eerst met Node serverside te gaan programmeren. Ik ga de Data Selfie App nabouwen, dit is een tutorialproject geschreven door
+Joey K. Lee.
+
+## Packages
+
+- Express
---
+++ 08.Serverside-Node/index.js
@@ -0,0 +1,5 @@
+// Met het require sleutelwoord stop je makkelijk een bestand of in dit geval een complete map in een constante, hierna kan je de functionaliteit
+// van wat je in de parameter ingevuld hebt gebruiken.
+const express = require("express");
+const app = express();
+app.listen(3000, () => console.log("Listening...."));
